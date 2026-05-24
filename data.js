1910f95cbde8ed6a9fd6ed6472d0a414d931ca0f
emerald32-source <emerald320032@gmail.com>
2026-05-24 12:10:19 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,68 @@
+const clientCustomName = "كوكو و تيا";
+const clientGender = "f";
+const clientEidiyaCode = "EID-500-VIP";
+const clientEnvelopeText = `كل عام وانت بكل الخير يا كوكو🥰🥰
+كل عام وانت بكل الخير يا توتا والف سلامه عليكي 🥰🥰
+عيدكم مبارك ❤️`;
+const clientMemories = [
+    {
+        "photo": "photo1.jpg",
+        "caption": "عيدكم مبارك"
+    },
+    {
+        "photo": "photo2.jpg",
+        "caption": "قلب عمتو"
+    },
+    {
+        "photo": "photo3.jpg",
+        "caption": "ربي يحفظك"
+    },
+    {
+        "photo": "photo4.jpg",
+        "caption": "قلب عمتو"
+    },
+    {
+        "photo": "photo5.jpg",
+        "caption": "حبيبة قلبي"
+    },
+    {
+        "photo": "photo6.jpg",
+        "caption": "اجمل بنوته"
+    },
+    {
+        "photo": "photo7.jpg",
+        "caption": "احلى البنات"
+    },
+    {
+        "photo": "photo8.jpg",
+        "caption": "حبيبة عمتو"
+    },
+    {
+        "photo": "photo9.jpg",
+        "caption": "ربي يحفظك"
+    },
+    {
+        "photo": "photo10.jpg",
+        "caption": "اجمل واحده"
+    },
+    {
+        "photo": "photo11.jpg",
+        "caption": "اشطر بنوته"
+    },
+    {
+        "photo": "photo12.jpg",
+        "caption": "احلى الحلوين"
+    },
+    {
+        "photo": "photo13.jpg",
+        "caption": "قمورهههه"
+    },
+    {
+        "photo": "photo14.jpg",
+        "caption": "حلو لا تعليق"
+    },
+    {
+        "photo": "photo15.jpg",
+        "caption": "جميلللل"
+    }
+];
